d145a8782f1cb3fc76aa240bc77b023207526b15
background58 <kimozaur@gmail.com>
2016-09-03 00:18:25 +0300

    Update canary.php
    #RO translate

--- view/template/page/canary.php
+++ view/template/page/canary.php
@@ -2,20 +2,20 @@
 <?php echo View::render('nav/_header', ['isDark' => false ]) ?>
   <main>
     <div class="content">
-      <h1>LBRY Warrant Canary</h1>
+      <h1>LBRY mandat Canary</h1>
       <p>
-        Through September 1st, 2017, LBRY has received:
+        Pe 1 Septembrie, 2017, LBRY a primit:
       </p>
       <ul>
-        <li>Zero National Security Letters</li>
-        <li>Zero Foreign Intelligence Surveillance Court orders</li>
-        <li>Zero gag orders that prevent us from stating we have received legal process seeking user or visitor information.</li>
+        <li>Zero Scrisori naționale de securitate</li>
+        <li>Zero Supravegherea de informații externe și ordine judecătorești</li>
+        <li>Zero ordinele de gag , care ne împiedică să declarând ca am primit un proces legal care caută informații de utilizator sau vizitator .</li>
       </ul>
       <p>
-        The date on this page will be updated every 3 months.
+        Data de pe această pagină va fi actualizată la fiecare 3 luni.
       </p>
       <p>
-        You can view the source code and revision history of this page
+        Puteți vizualiza codul sursă și istoricul de revizuire a acestei pagini
         <a href="https://github.com/lbryio/lbry.io/commits/master/view/template/page/canary.php" class="link-primary">here</a>.
       </p>
       <p>
